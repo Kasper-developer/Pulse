5b16e5bebe698bd9fdd1d13de06dfb80009c73d3
Kasper <mirzohid.rahmatullaev@gmail.com>
2021-08-15 02:07:15 +0500

    modified: optimized index.html

--- src/js/main.js
+++ src/js/main.js
@@ -209,7 +209,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
-	$("a[href^='#']").click(function () {
+	$("a[href = '#up'], a[href = '#catalog']").click(function () {
 		const _href = $(this).attr("href");
 		$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
 		return false;
